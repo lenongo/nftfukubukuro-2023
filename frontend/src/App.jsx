6df617336fc752733856d9291a9d6dc55f5ce9e9
shun <funakki0904@gmail.com>
2022-09-23 11:06:37 +0900

    "AIDAO用変更箇所"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="background">
       <div className="container">
-        <img width="200" height="200" src="/config/images/logo-win.png" />
+        <img width="200" height="200" src="/config/images/example.gif" />
         <MintPage />
       </div>
     </div>
